7604a4b10016cfe3228094df6057dd778d99a249
mika wu <wu.wenwen@gmail.com>
2025-06-09 23:18:07 -0700

    fourth commit win scene now tells you you can restart

--- src/Scenes/WinScene.js
+++ src/Scenes/WinScene.js
@@ -6,6 +6,7 @@ super('winScene');
 
 create() {
     this.add.text(400, 300, 'YOU WIN!', {font: '48px', fill: '#fff'}).setOrigin(0.5);
+    this.add.text( 400,340, 'Press SPACE to Restart', {fontSize: '24px', fill: '#fff'}).setOrigin(0.5);
 
     this.input.keyboard.once('keydown-SPACE', () => {
         this.scene.start('galleryShooter');
